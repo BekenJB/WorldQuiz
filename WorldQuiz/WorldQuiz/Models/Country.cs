ace615272efb7872abba4a76c41c72e9f546efca
Bekenty Jean Baptiste <bekenty.baptiste@outlook.com>
2018-04-22 20:45:51 -0400

    Checked in basic folders needed for this app after getting it to run successfully on my local.

---
+++ WorldQuiz/WorldQuiz/Models/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldQuiz.Models
+{
+    public class Country
+    {
+
+        public List<Currency> Currencies { get; set; }
+
+        public string Name { get; set; }
+
+        public string Capital { get; set; }
+    }
+}
---
+++ WorldQuiz/WorldQuiz/Models/Currency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldQuiz.Models
+{
+    public class Currency
+    {
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string Symbol { get; set; }
+    }
+}
